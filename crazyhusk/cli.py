6e50fccbf24101cfe8ab7758f9346469dd43bb7a
nhaines-pro <nhaines.pro@gmail.com>
2022-02-01 13:17:43 -0800

    add dynamic cli command loading

--- crazyhusk/cli.py
+++ crazyhusk/cli.py
@@ -1,9 +1,21 @@
 """Expose crazyhusk functionality to the commandline."""
 import argparse
+import pkg_resources
 
 
 def run():
     """Run the crazyhusk CLI entrypoint."""
     parser = argparse.ArgumentParser()
+    commands_parser = parser.add_subparsers(help='sub-command help',
+                                            dest='command')
+
+    commands = {}
+    for entry_point in pkg_resources.iter_entry_points('crazyhusk.commands'):
+        commands[entry_point.name] = entry_point.load()
+        command_parser = commands_parser.add_parser(entry_point.name)
+
     args = parser.parse_args()
-    print(args)
+
+    command = commands.get(args.command)
+    if command is not None:
+        command()

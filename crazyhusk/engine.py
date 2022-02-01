74e5ed41c925699dd4bb0afad28e2b4682897fc8
nhaines-pro <nhaines.pro@gmail.com>
2022-02-01 14:21:42 -0800

    find windows registry engines

--- crazyhusk/engine.py
+++ crazyhusk/engine.py
@@ -3,11 +3,12 @@ import glob
 import json
 import os
 import platform
+import winreg
 import pkg_resources
 
 
 def find_egl_engines_windows():
-    """Find and yeild all Epic Games Launcher engines."""
+    """Find and yield all Epic Games Launcher engines."""
     if platform.system() != "Windows":
         return
     print("Finding Epic Games Launcher installations for Windows platform...")
@@ -20,6 +21,23 @@ def find_egl_engines_windows():
     # check legacy paths
     for ue_dir in glob.iglob(r"C:\Program Files\Epic Games\UE_*"):
         yield ue_dir
+
+
+def find_registered_engines_windows():
+    """Find and yield all engines associated via Windows Registry keys."""
+    if platform.system() != "Windows":
+        return
+
+    print("Finding Windows Registry installations...")
+    try:
+        with winreg.OpenKey(
+            winreg.HKEY_CURRENT_USER, r"Software\Epic Games\Unreal Engine\Builds"
+        ) as key:
+            for i in range(1024):
+                _key, _value, _type = winreg.EnumValue(key, i)
+                yield os.path.abspath(os.path.join(_value, "Engine"))
+    except OSError:
+        return
 
 
 def list_engines():

8df8e70f95eba45ad4c927dfa98f3cefbfeb389f
nhaines-pro <nhaines.pro@gmail.com>
2022-02-08 17:13:31 -0800

    Dynamic sub-command argument parsing Fixes #4

--- crazyhusk/cli.py
+++ crazyhusk/cli.py
@@ -1,6 +1,7 @@
 """Expose crazyhusk functionality to the commandline."""
 # Standard Library
 import argparse
+import inspect
 import logging
 import sys
 
@@ -8,20 +9,54 @@ import sys
 import pkg_resources
 
 
+def set_subcommand_arguments(parser, command):
+    """Dynamically set argparse.Parser subcommand arguments by inspecting a callable function."""
+    fullargs = inspect.getfullargspec(command)
+
+    if fullargs.defaults:
+        firstdefault = len(fullargs.args) - len(fullargs.defaults)
+
+    for i, arg in enumerate(fullargs.args):
+        if fullargs.defaults and i >= firstdefault:
+            default = fullargs.defaults[i - firstdefault]
+            if default is None:
+                parser.add_argument(dest=arg)
+            elif type(default) is tuple or type(default) is list:
+                parser.add_argument(
+                    "--" + arg, default=default, type=type(default), nargs="+"
+                )
+            elif type(default) is bool and default:
+                parser.add_argument(
+                    "--disable_" + arg, dest=arg, default=default, action="store_false"
+                )
+            elif type(default) is bool and not default:
+                parser.add_argument(
+                    "--enable_" + arg, dest=arg, default=default, action="store_true"
+                )
+            else:
+                parser.add_argument("--" + arg, default=default, type=type(default))
+        else:
+            parser.add_argument(dest=arg)
+
+    return parser
+
+
 def run():
     """Run the crazyhusk CLI entrypoint."""
     logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
     parser = argparse.ArgumentParser()
-    commands_parser = parser.add_subparsers(help="sub-command help", dest="command")
+    commands_parser = parser.add_subparsers(
+        title="subcommands", description="valid subcommands", help="subcommand help"
+    )
 
-    commands = {}
     for entry_point in pkg_resources.iter_entry_points("crazyhusk.commands"):
-        commands[entry_point.name] = entry_point.load()
-        commands_parser.add_parser(entry_point.name)
+        command = entry_point.load()
+        if inspect.isfunction(command):
+            cmd_parser = commands_parser.add_parser(entry_point.name)
+            cmd_parser.set_defaults(command=command)
+            set_subcommand_arguments(cmd_parser, command)
 
     args = parser.parse_args()
-
-    command = commands.get(args.command)
-    if command is not None:
-        command()
+    if "command" in args:
+        args.command(**{k: v for k, v in args.__dict__.items() if k != "command"})

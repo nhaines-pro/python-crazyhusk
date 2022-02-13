18cc7f01dec9738eb33940d3d5db3c54aaa538ea
nhaines-pro <nhaines.pro@gmail.com>
2022-02-12 17:21:58 -0800

    try to fix windows coverage paths

---
+++ ci/fix_win_coverage_paths.py
@@ -0,0 +1,15 @@
+from contextlib import closing
+import glob
+import sqlite3
+
+
+def main():
+    for coverage_db in glob.iglob("./.coverage.*"):
+        with closing(sqlite3.connect(coverage_db)) as connection:
+            with closing(connection.cursor()) as cursor:
+                cursor.execute("UPDATE file SET path=REPLACE(path,'\\','/')")
+                connection.commit()
+
+
+if __name__ == "__main__":
+    main()

c70d65f4966a1def5eea1f4a9a457e0f1762d880
nhaines-pro <nhaines.pro@gmail.com>
2022-02-14 16:26:41 -0800

    coverage for __main__

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,3 +3,22 @@ import pytest
 
 # CrazyHusk
 from crazyhusk.__main__ import main
+from crazyhusk import cli
+
+@pytest.mark.parametrize(
+    "args,raises",
+    [
+        (None, (cli.CommandError, SystemExit, TypeError)),
+        ([], (cli.CommandError, SystemExit)),
+        (["test-command"], (cli.CommandError, SystemExit)),
+        ([""], (cli.CommandError, SystemExit)),
+        # TODO: monkeypatch pkg_resources behavior
+    ],
+)
+def test_main(monkeypatch, args, raises):
+    monkeypatch.setattr("sys.argv", args)
+    if raises is not None:
+        with pytest.raises(raises):
+            assert main()
+    else:
+        assert main()

27a7e439f5b323b89924a4872f5717beaf537290
nhaines-pro <nhaines.pro@gmail.com>
2022-02-22 14:50:05 -0800

    more windows.engine test coverage

--- tests/test_windows.py
+++ tests/test_windows.py
@@ -28,6 +28,11 @@ def basic_datfile(tmp_path):
     )
 
 
+@pytest.fixture(scope="function")
+def engine_local():
+    yield engine.UnrealEngine(".")
+
+
 def test_find_egl_engine_windows_no_datfile(tmp_path, monkeypatch):
     dat_file = tmp_path / "LauncherInstalled.dat"
     engine.DAT_FILE = dat_file
@@ -77,3 +82,48 @@ def test_registry_nonwindows(monkeypatch):
     monkeypatch.setattr("platform.system", lambda: "NotWindows")
     assert engine.find_registered_engines_windows("random_string") is None
     assert len(list(engine.list_registered_engines_windows())) == 0
+
+
+def test_registry_windows(monkeypatch):
+    monkeypatch.setattr("platform.system", lambda: "Windows")
+    assert engine.find_registered_engines_windows("random_string") is None
+    assert len(list(engine.list_registered_engines_windows())) == 0
+
+
+def test_resolve_executable_path_windows(monkeypatch, engine_local):
+    monkeypatch.setattr("platform.system", lambda: "NotWindows")
+    assert engine.resolve_executable_path_windows(None, None) is None
+    monkeypatch.setattr("platform.system", lambda: "Windows")
+    with pytest.raises(TypeError):
+        assert engine.resolve_executable_path_windows(None, None) is None
+
+    with pytest.raises(AttributeError):
+        assert engine.resolve_executable_path_windows(engine_local, None) is None
+
+    assert engine.resolve_executable_path_windows(engine_local, "") is None
+    assert engine.resolve_executable_path_windows(engine_local, "random_string") is None
+    assert (
+        engine.resolve_executable_path_windows(engine_local, "AutomationTool")
+        is not None
+    )
+    assert (
+        engine.resolve_executable_path_windows(engine_local, "automationtool")
+        is not None
+    )
+    assert (
+        engine.resolve_executable_path_windows(engine_local, "swarmagent") is not None
+    )
+    assert (
+        engine.resolve_executable_path_windows(engine_local, "swarmcoordinator")
+        is not None
+    )
+    assert (
+        engine.resolve_executable_path_windows(engine_local, "unrealbuildtool")
+        is not None
+    )
+    assert engine.resolve_executable_path_windows(engine_local, "ue4editor") is not None
+    assert (
+        engine.resolve_executable_path_windows(engine_local, "ue4editor-cmd")
+        is not None
+    )
+    assert engine.resolve_executable_path_windows(engine_local, "unrealpak") is not None

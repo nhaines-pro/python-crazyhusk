5466ca2b7be540f2b5c58260a7e21d9a81fb047f
nhaines-pro <nhaines.pro@gmail.com>
2022-03-01 16:47:16 -0800

    more autodoc configs

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,6 @@
 """Sphinx configuration."""
+from importlib import metadata as md
+
 project = "CrazyHusk"
 author = "Nick Haines"
 copyright = "2022, Nick Haines"
@@ -7,6 +9,18 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx_click",
     "myst_parser",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+    "sphinx.ext.coverage",
+    "sphinx.ext.ifconfig",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.mathjax",
 ]
 autodoc_typehints = "description"
 html_theme = "furo"
+
+basename = "crazyhusk"
+_metadata = md.metadata(basename)
+release = _metadata.get("Version")
+version = ".".join(release.split(".")[:2])
